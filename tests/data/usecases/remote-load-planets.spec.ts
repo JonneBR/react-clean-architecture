a495685aa40b8a299ab5cfd120ba72c5d35f46c3
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2023-01-05 14:24:37 -0300

    feat: adding new test

--- tests/data/usecases/remote-load-planets.spec.ts
+++ tests/data/usecases/remote-load-planets.spec.ts
@@ -1,10 +1,15 @@
 import { RemoteLoadPlanets } from '../../../src/data/usecases/remote-load-planets'
+import { LoadPlanets } from '../../../src/domain/usecases'
 import { HttpGetClientSpy } from '../mocks'
+import { mockRemoteLoadPlanetsListModel } from '../mocks/mock-load-planets'
 
-type SutTypes = { sut: RemoteLoadPlanets; httpGetClientSpy: HttpGetClientSpy }
+type SutTypes = {
+  sut: RemoteLoadPlanets
+  httpGetClientSpy: HttpGetClientSpy<LoadPlanets.Model[]>
+}
 
 const makeSut = (url = 'any_url'): SutTypes => {
-  const httpGetClientSpy = new HttpGetClientSpy()
+  const httpGetClientSpy = new HttpGetClientSpy<LoadPlanets.Model[]>()
   const sut = new RemoteLoadPlanets(url, httpGetClientSpy)
   return { sut, httpGetClientSpy }
 }
@@ -16,4 +21,24 @@ test('Should call HttpGetClient with correct URL and Method', async () => {
 
   expect(httpGetClientSpy.url).toBe(url)
   expect(httpGetClientSpy.method).toBe('get')
+})
+
+test('Should call HttpGetClient and returns 200', async () => {
+  const { sut, httpGetClientSpy } = makeSut()
+
+  await sut.loadAll()
+
+  expect(httpGetClientSpy.response.statusCode).toBe(200)
+})
+
+test('Should return a list of Planets if HttpGetClient and returns 200', async () => {
+  const { sut, httpGetClientSpy } = makeSut()
+  const httpResult = mockRemoteLoadPlanetsListModel()
+  httpGetClientSpy.response = {
+    statusCode: 200,
+    body: httpResult,
+  }
+  const planetsList = await sut.loadAll()
+
+  expect(planetsList).toEqual(httpResult)
 })
